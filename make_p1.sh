5778cd69633a03179aa971158bb154acb80f4118
fernan <fernanbolando@gmail.com>
2024-09-08 10:17:43 -0500

    add build scripts. in case developer pulls the rug again I will at least have clues.

---
+++ make_p1.sh
@@ -0,0 +1,3 @@
+## arduino-cli  Version: 1.0.4 Commit: a0d912da Date: 2024-08-12T13:42:36Z
+## esp8266:esp8266 3.1.2   /home/fernan/.arduino15/packages/esp8266/hardware/esp8266/3.1.2
+arduino-cli -v compile --fqbn esp8266:esp8266:nodemcu p1
